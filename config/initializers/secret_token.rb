78caad9129788acdf6a1e6c2ad710ef988bd70d7
ubuntu <ubuntu@ubuntu-ThinkPad-T410.(none)>
2013-01-14 22:53:54 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZeroToDeploy::Application.config.secret_token = '74d169da14023bcae66ca104340b5ff00f7f8662b37e0ee59b629083493ca097163426ae43204ae234a9dddd13aeca34ffd2746304029e69b075fdb32b61e182'
